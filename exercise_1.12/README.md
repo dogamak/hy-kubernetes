31b488fb3309830d0b5f1dadb2b51dd4f06aa5bb
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2022-06-27 23:22:40 +0300

    Exercise 1.12

---
+++ exercise_1.12/README.md
@@ -0,0 +1,8 @@
+# Exercise 1.08: Project v0.6
+
+```shell
+$ docker build . -t dogamak/hy-kubernetes-todo-app:v0.6
+$ docker push dogamak/hy-kubernetes-todo-app:v0.6
+$ kubectl apply -f manifests/
+$ firefox http://localhost:8081/
+```
---
+++ exercise_1.12/frontend/src/App.js
@@ -0,0 +1,41 @@
+import logo from '!file-loader!./kubernetes.svg'; // eslint-disable-line
+import './App.css';
+import { useGetPictureOfTheDayQuery } from './api/pictureOfTheDay'
+
+const PictureOfTheDay = () => {
+  const { data, isLoading, isError } = useGetPictureOfTheDayQuery()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error!</div>
+  }
+
+  const { url, last_updated } = data
+
+  return (
+    <div>
+      <img className="picture-of-the-day" src={url} />
+      <p>Last updated at: {last_updated}</p>
+    </div>
+  )
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, Kubernetes!
+        </p>
+        <h1>Picture of the day:</h1>
+        <PictureOfTheDay />
+      </header>
+    </div>
+  );
+}
+
+export default App;
